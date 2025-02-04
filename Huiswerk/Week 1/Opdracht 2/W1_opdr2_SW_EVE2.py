b24d772710c8360c301c7776638796414a87e6da
Benjahmin <benjahminhekkers@outlook.com>
2025-02-04 15:06:27 +0100

    Opdracht 2 af

--- Huiswerk/Week 1/Opdracht 2/W1_opdr2_SW_EVE2.py
+++ Huiswerk/Week 1/Opdracht 2/W1_opdr2_SW_EVE2.py
@@ -7,24 +7,24 @@ class boeken:
 
     def leen_uit(self):
         if self.uitgeleend == False:
-            print("Helaas het boek is uitgeleend")
-        else:
             self.uitgeleend = True
-            print("Bedankt voor het lenen van het boek, veel lees plezier!")
+            print(f"Bedankt voor het lenen van {self.titel}, veel lees plezier!")
+        else:
+            print(f"Fout Het boek {self.titel} is al uitgeleend.")
 
     def breng_terug(self):
         if self.uitgeleend == True:
-            print("Bedankt voor het terug brengen van het boek!")
             self.uitgeleend = False
+            print("Bedankt voor het terug brengen van het boek!")
         else:
-            print(f"Fout Het boek {self.titel} is uitgeleend.")
+            print(f"Fout Het boek {self.titel} is niet uitgeleend.")
 
     def status(self):
-        if self.uitgeleend == True:
-            status = "Ja"
-        else:
+        if self.uitgeleend == False:
             status = "Nee"
+        else:
+            status = "Ja"
         return status
 
     def __str__(self):
-        return F"Titel {self.titel}, auteur {self.auteur}, jaar {self.jaar}, uitgeleend: {self.status}"
+        return F"Titel {self.titel}, auteur {self.auteur}, jaar {self.jaar}, uitgeleend: {self.status()}"
--- Huiswerk/Week 1/Opdracht 2/test_boek.py
+++ Huiswerk/Week 1/Opdracht 2/test_boek.py
@@ -2,6 +2,11 @@ from W1_opdr2_SW_EVE2 import boeken
 
 boek1 = boeken("De Hobbit", "J.R.R. Tolkien", 1937)
 
-boek1.leen_uit
 print(boek1)
-
+boek1.leen_uit()
+print(boek1)
+boek1.leen_uit()
+boek1.breng_terug()
+print(boek1)
+boek1.breng_terug()
+print(boek1)
