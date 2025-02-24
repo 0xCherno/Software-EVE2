aa030de4de85c55924a9997a9de90bbec7633da0
Benjahmin <benjahminhekkers@outlook.com>
2025-02-24 13:43:52 +0100

    Week 1 af

--- DP7_recepten/Week 1/main.py
+++ DP7_recepten/Week 1/main.py
@@ -51,8 +51,25 @@ def main():
     recepten.append(Anabolische_Kwark)
     recepten.append(Kip_Rijst_Brocoli)
 
-    for recept in recepten:
-        print(recept)
+    while True:
+        print("\n Beschikabre recepten:")
+        for i, recept in enumerate(recepten, start=1):
+            print(f"{i}. {recept.get_naam()}")
+        
+        keuze = input("\nVoer het nummer in van het recept dat je wilt bekijken (Of q om het programma af te sluiten):")
+
+        if keuze.lower() == "q":
+            print("Programma afgesloten")
+            break
+
+        try:
+            keuze_index = int(keuze) - 1
+            if 0 <= keuze_index < len(recepten):
+                print("\n" + str(recepten[keuze_index]))
+            else:
+                print("Ongeldige invoer, kies een nummer uit de lijst.")
+        except ValueError:
+            print("Voer een geldig nummer in.")
 
 if __name__ == "__main__":
     main()
