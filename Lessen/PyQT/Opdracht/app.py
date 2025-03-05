02e42cda302d0d214d72da5662b1b5db319a5e85
Benjahmin Hekkers s1216563 <s1216563@student.windesheim.nl>
2025-03-05 13:51:37 +0100

    output venster toegevoegd, en knoppen + facts werkend

--- Lessen/PyQT/Opdracht/app.py
+++ Lessen/PyQT/Opdracht/app.py
@@ -1,14 +1,11 @@
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
-from random import choice
-
-#Importeren van de lijsten
 from feiten_eng import Facts
 from feiten_nl import Feiten
 from feiten_frans import Faits
 from feiten_duits import Fakten
-
+import random
 
 feiten_eng = Facts
 feiten_nl = Feiten
@@ -19,8 +16,7 @@ class MijnScherm(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("4 Knoppen & 50 feitjes!")
-        self.x_keren_ingedrukt = 0
+        self.setWindowTitle("4 Knoppen, 4 Talen & 50 feitjes!")
 
         self.knop_engels = QPushButton("Generate fact")
         self.knop_engels.clicked.connect(self.knop_ingedrukt)
@@ -32,28 +28,46 @@ class MijnScherm(QWidget):
         self.knop_duits.move(64,64)
         self.knop_duits.setFixedSize(200, 200)
 
-        self.knop_frans = QPushButton("générer des faits")
+        self.knop_frans = QPushButton("Générer des faits")
         self.knop_frans.clicked.connect(self.knop_ingedrukt)
         self.knop_frans.move(128,32)
         self.knop_frans.setFixedSize(200, 200)
 
-        self.knop_nederlands = QPushButton("genereer feit")
+        self.knop_nederlands = QPushButton("Genereer feit")
         self.knop_nederlands.clicked.connect(self.knop_ingedrukt)
         self.knop_nederlands.move(128,64)
         self.knop_nederlands.setFixedSize(200, 200)
 
+        self.output_venster = QTextEdit(self)
+        self.output_venster.move(250, 600)
+        # self.output_venster.setFixedSize(400, 200)
+        self.output_venster.setPlaceholderText("Output")
+
         self.setFixedSize(500, 700)
         
         layout = QGridLayout()
-        layout.addWidget(self.knop_engels, 2,0)
-        layout.addWidget(self.knop_nederlands, 2, 1)
-        layout.addWidget(self.knop_duits, 3, 0)
-        layout.addWidget(self.knop_frans, 3, 1)
+        layout.addWidget(self.knop_engels, 1,0)
+        layout.addWidget(self.knop_nederlands, 1, 1)
+        layout.addWidget(self.knop_duits, 2, 0)
+        layout.addWidget(self.knop_frans, 2, 1)
+        layout.addWidget(self.output_venster, 3, 0, 1, 2)
         self.setLayout(layout)
     
     def knop_ingedrukt(self):
-        print("Ingedrukt")
+        self.sender()
+        knop_naam = self.sender().text()
 
+        if knop_naam == "Generate fact":
+            print(f"{random.choice(feiten_eng)}\n")
+        elif knop_naam == 'Fakten erzeugen':
+            print(f"{random.choice(feiten_duits)}\n")
+        elif knop_naam == 'Générer des faits':
+            print(f"{random.choice(feiten_frans)}\n")
+        elif knop_naam == 'Genereer feit':
+            print(f"{random.choice(feiten_nl)}\n")
+        else:
+            print(f"Knop niet herkend {knop_naam}")
+        return
 
 app = QApplication([])
 scherm = MijnScherm()
