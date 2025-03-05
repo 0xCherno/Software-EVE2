31d5647e8b6953ba7909af58aea1c266730a5bb8
Benjahmin Hekkers s1216563 <s1216563@student.windesheim.nl>
2025-03-05 11:05:30 +0100

    button acties

---
+++ Lessen/PyQT/Opdracht/app.py
@@ -0,0 +1,47 @@
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from random import choice
+from facts import Facts
+
+feiten = Facts
+
+class MijnScherm(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("4 Knoppen & 50 feitjes!")
+        self.x_keren_ingedrukt = 0
+
+        self.feit_engels = QPushButton("fact")
+        self.feit_engels.clicked.connect(self.knop_is_ingedrukt)
+
+        self.feit_duits = QPushButton("Tatsache")
+        self.feit_duits.clicked.connect(self.knop_is_ingedrukt)
+
+        self.feit_frans = QPushButton("fait")
+        self.feit_frans.clicked.connect(self.knop_is_ingedrukt)
+
+        self.feit_nederlands = QPushButton("feit")
+        self.feit_nederlands.clicked.connect(self.knop_is_ingedrukt)
+
+        self.setCentralWidget(self.)
+        self.setFixedSize(500, 500)
+
+    def knop_is_ingedrukt(self):
+        print("Ingedrukt")
+        nieuwe_scherm_titel = choice(scherm_titels)
+        print(f"Nieuwe titel is: {nieuwe_scherm_titel}")
+        self.setWindowTitle(nieuwe_scherm_titel)
+    
+    def scherm_titel_gewijzigd(self, scherm_titel):
+        print(f"De titel van het scherm is gewijzigd: {scherm_titel}")
+
+        if scherm_titel == 'error':
+            self.knop.setDisabled(True)
+            print("Error: something went wrong, button disabled")
+
+app = QApplication([])
+scherm = MijnScherm()
+scherm.show()
+app.exec()
