fac14a1e1f55c8af3ca672354bf60595ece9af88
Benjahmin Hekkers s1216563 <s1216563@student.windesheim.nl>
2025-03-05 09:27:21 +0100

    PyQT tutorial volgen stap 1 af

--- Lessen/PyQT/pyqt.py
+++ Lessen/PyQT/pyqt.py
@@ -2,18 +2,22 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 
+class MijnScherm(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Restaurant Lake Side Mania")
+        knop = QPushButton("Bestellen")
+        knop.setCheckable(True)
+        knop.clicked.connect(self.knop_ingedrukt)
 
+        self.setCentralWidget(knop)
+        self.setFixedSize(500, 500)
 
-
-
-
-
-
-
-
-
+    def knop_ingedrukt(self):
+        print("Knop ingedrukt!")
 
 app = QApplication([])
-scherm = QMainWindow()
+scherm = MijnScherm()
 scherm.show()
 app.exec()
