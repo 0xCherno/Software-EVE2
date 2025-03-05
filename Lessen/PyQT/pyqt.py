204ee6aefefadc807f8deb59ccabd0d3afeb5579
Benjahmin Hekkers s1216563 <s1216563@student.windesheim.nl>
2025-03-05 10:10:26 +0100

    button checkable

--- Lessen/PyQT/pyqt.py
+++ Lessen/PyQT/pyqt.py
@@ -6,16 +6,20 @@ class MijnScherm(QMainWindow):
     def __init__(self):
         super().__init__()
         
+        self.knop_is_checked = False
         self.setWindowTitle("Restaurant Lake Side Mania")
-        knop = QPushButton("Bestellen")
-        knop.setCheckable(True)
-        knop.clicked.connect(self.knop_ingedrukt)
 
-        self.setCentralWidget(knop)
+        self.knop = QPushButton("Press me!")
+        self.knop.setCheckable(True)
+        self.knop.released.connect(self.knop_is_los)
+        self.knop.setChecked(self.knop_is_checked)
+
+        self.setCentralWidget(self.knop)
         self.setFixedSize(500, 500)
 
-    def knop_ingedrukt(self):
-        print("Knop ingedrukt!")
+    def knop_is_los(self):
+        self.knop_is_checked = self.knop.isChecked()
+        print(self.knop_is_checked)
 
 app = QApplication([])
 scherm = MijnScherm()
